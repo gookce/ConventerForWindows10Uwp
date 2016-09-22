ae9f5a1911219586ecabce086fe135c13aaea631
Gokce Demir <gookcedemir@gmail.com>
2016-09-22 13:49:28 +0300

    finished

---
+++ MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Conventer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker imagePicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary,
+                FileTypeFilter = { ".jpg", ".png", ".bmp", ".gif", ".tif" }
+            };
+
+            StorageFile file = await imagePicker.PickSingleFileAsync();
+
+            var stream = await file.OpenStreamForReadAsync();
+            var byteArray = new byte[(int)stream.Length];
+            await stream.ReadAsync(byteArray, 0, (int)stream.Length);
+
+            BitmapImage image = await ToImage(byteArray);
+            banner.Source = image;
+        }
+
+        public async Task<byte[]> ToByte (StorageFile file)
+        {
+            using (var inputStream = await file.OpenSequentialReadAsync())
+            {
+                var readStream = inputStream.AsStreamForRead();
+                byte[] buffer = new byte[readStream.Length];
+                await readStream.WriteAsync(buffer, 0, buffer.Length);
+                return buffer;
+            }
+        }
+
+        public async Task<BitmapImage> ToImage (byte[] array)
+        {
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                using (DataWriter writer = new DataWriter(stream.GetOutputStreamAt(0)))
+                {
+                    writer.WriteBytes(array);
+                    await writer.StoreAsync();
+                }
+                BitmapImage image = new BitmapImage();
+                await image.SetSourceAsync(stream);
+                return image;
+            }
+        }
+    }
+}
